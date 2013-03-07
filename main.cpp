a4c8fedf35c824cfae25d9e32b75a8fb7686e29f
Silence-T430 <marcel@tbi.univie.ac.at>
2013-03-07 11:33:14 +0100

    Minor change in time output

--- main.cpp
+++ main.cpp
@@ -33,10 +33,11 @@ int main(int argc, char **argv)
     TBD output;
     dsu.Cluster(args_info.cluster_Kmax_arg, output);
     dsu.ComputeTBD(output, args_info.depth_arg, args_info.num_threshold_arg, args_info.outer_flag, args_info.noLP_flag, args_info.shift_flag, args_info.debug_flag);
-    fprintf(stderr, "clustering took %.2f secs.\n", (clock()-time)/(double)CLOCKS_PER_SEC); time = clock();
+    fprintf(stderr, "clustering took %.2f secs.\n", (clock()-time)/(double)CLOCKS_PER_SEC);
   } else {
     dsu.CreateList(args_info.hd_threshold_arg, args_info.debug_flag);
     dsu.ComputeUB(args_info.depth_arg, args_info.num_threshold_arg, args_info.outer_flag, args_info.noLP_flag, args_info.shift_flag, args_info.include_higher_flag, args_info.debug_flag);
+    fprintf(stderr, "computation of UB list took %.2f secs.\n", (clock()-time)/(double)CLOCKS_PER_SEC);
   }
   //dsu.PrintUBoutput(stderr);
 
@@ -44,7 +45,7 @@ int main(int argc, char **argv)
     printf("%.2f %d\n", (clock()-time)/(double)CLOCKS_PER_SEC, dsu.Size()); time = clock();
 
   } else {
-    fprintf(stderr, "computation of UB list took %.2f secs.\n", (clock()-time)/(double)CLOCKS_PER_SEC); time = clock();
+    time = clock();
       // LinkCP
     Opt opt(args_info.noLP_flag, args_info.shift_flag, !args_info.noSaddle_flag, args_info.floodMax_arg, args_info.floodHeight_arg);
     dsu.LinkCPLM(opt, args_info.debug_flag);
